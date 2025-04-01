9dd96f35727872da883bb88255c7fa94cd7731f7
victoryalhj <victoryalhj@gmail.com>
2025-04-01 22:59:53 +0100

    feat: result and color add

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Box from './component/Box';
 
 //1.박스 2개 그리기(title,img,result)
 //2.가위바위보 button 보여주기
-//3.button을 클릭하면 클릭한 값이 보임-나
+//3.button을 클릭하면 클릭한 값이 보임-You
 //4.computer는 랜덤하게 item선택이됨-computer
 //5.3&4번의 결과를 가지고 누가이겼는지 승패를 나눈다
 //6.승패결과에따라 테두리색이 바뀐다(win-green,lose-red,draw-black)
+//컴퓨터 결과보이게하기 
+
 
 //사진을 저장할수 있는 객체아이템
 const choice = {
@@ -29,16 +31,48 @@ const choice = {
 
 function App() {
   const [userSelect, setUserSelect] = useState(null)
-
+  //computer state
+  const [computerSelect,setComputerSelect] = useState(null)
+  //win&lose
+  const [result,setResult] = useState("")
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]) 
-  }
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice],computerChoice));
+    };
+
+    const judgement=(user,computer)=> {
+      console.log("user",user,"computer",computer)
+
+      //game logic
+      //user==computer draw
+      //lose rock < paper scissor < rock paper < scissor
+      //win rock > scissor scissor > paper paper > rock 
+      if(user.name == computer.name) {
+        return "draw"
+      }else if (user.name == "Rock")return computer.name=="Scissors"?"win":"lose";
+      else if (user.name =="Scissors")return computer.name=="Parer"?"win":"lose";
+      else if (user.name == "Paper")return computer.name=="Rock"?"win":"lose";
+    };
+
+    const randomChoice = () => {
+      //객체 배열화
+      let itemArray = Object.keys(choice);
+      console.log("itemArray",itemArray)
+      let randomItem = Math.floor(Math.random()*itemArray.length);
+      let final = itemArray[randomItem];
+      console.log("final",final)
+      return choice[final];
+    }
+
+
   return ( 
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computer"/> */}
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>Scissors</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -2,13 +2,22 @@ import React from "react";
 
 const Box = (props) => {
   console.log("props",props)
+
+  let result;
+  if (props.title === "Computer" && props.result !== "draw" && props.result !== "") {
+    result = props.result === "win"?"lose":"win";
+    }else {
+      result = props.result;
+    }
+
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
+      {/* <h2>{props.item && props.item.name}</h2> */}
       <img
         className="item-image"
         src={props.item && props.item.image} />
-      <h2>Win</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
